8b44d553bcb60516f82f01692c5ac217379eed22
stupid <sunfeng32@qq.com>
2017-09-12 22:42:43 +0800

    feat(RelationGraph): demo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,78 @@
 import React, { Component } from 'react';
-import { Button } from 'antd-mobile';
+import _ from 'lodash';
+import { NavBar } from 'antd-mobile';
 
 import logo from './logo-maimai.png';
 import './App.css';
+import RelationGraph from './RelationGraph';
+
+const ME = 0;
+const nodes = _.fill(Array(100), 0).map((v, index) => ({
+  id: index,
+  draggable: true,
+  targetCount: 0,
+  sourceCount: 0,
+}));
+
+const categories = [{ name: '我' }, { name: '关注对象' }];
+
+const edges = [];
+
+function createEdge(source, target) {
+  source.targetCount += 1;
+  target.sourceCount += 1;
+  return {
+    source: source.id,
+    target: target.id,
+  };
+}
+
+nodes.forEach((source) => {
+  nodes.forEach((target) => {
+    if (target.id <= source.id) return;
+
+    if (source.id === ME || Math.random() > 0.99) {
+      const mutual = Math.random() < 0.5;
+      const reverse = Math.random() < 0.5;
+      if (mutual) {
+        edges.push(createEdge(source, target));
+        edges.push(createEdge(target, source));
+        edges.push({
+          source: source.id,
+          target: target.id,
+          mutual: true,
+        });
+      } else if (reverse) {
+        edges.push(createEdge(target, source));
+      } else {
+        edges.push(createEdge(source, target));
+      }
+    }
+  });
+});
+
+nodes.forEach((node) => {
+  if (node.id === ME) {
+    node.name = '我';
+    node.category = '我';
+  } else {
+    node.name = `关注${node.id}`;
+    node.category = '关注对象';
+  }
+});
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>我的人脉有多强?</h2>
-        </div>
-        <div>
-          <Button type="primary">Button</Button>
-        </div>
+        <NavBar
+          iconName={null}
+          mode="light"
+          leftContent={<img src={logo} className="App-logo" alt="logo" />}
+        >
+          我的人脉有多强?
+        </NavBar>
+        <RelationGraph nodes={nodes} edges={edges} categories={categories} />
         <p className="App-intro">
           脉脉用户画像-DoraHacks-xxxx团队倾情奉献
         </p>
---
+++ src/RelationGraph.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { compose, withHandlers, withStateHandlers } from 'recompose';
+
+import ReactEcharts from 'echarts-for-react';
+import { SegmentedControl } from 'antd-mobile';
+
+const unilateralFollow = '关注';
+const mutualFollow = '互相关注';
+const followCount = '关注数量';
+const fansCount = '粉丝数量';
+const edgeMeanings = [unilateralFollow, mutualFollow];
+const nodeSizeMeanings = [followCount, fansCount];
+
+const RelationGraph = ({ getOption, setEdgeFor, setSizeFor, edgeFor, sizeFor }) => (
+  <div>
+    <ReactEcharts
+      option={getOption()}
+      style={{height: '700px', width: '100%'}}
+      notMerge={true}
+      lazyUpdate={true}
+      theme={"theme_name"}
+    />
+    <p className="sub-title">关系线表示:</p>
+    <SegmentedControl
+      values={edgeMeanings}
+      selectedIndex={edgeFor}
+      onChange={setEdgeFor}
+    />
+    <p className="sub-title">球大小表示:</p>
+    <SegmentedControl
+      values={nodeSizeMeanings}
+      selectedIndex={sizeFor}
+      onChange={setSizeFor}
+    />
+  </div>
+);
+
+const getOption = ({ nodes, categories, edges, edgeFor, sizeFor }) => () => {
+  const wantMutual = edgeMeanings[edgeFor] === mutualFollow;
+  const wantFollowCount = nodeSizeMeanings[sizeFor] === followCount;
+  console.log(sizeFor, nodeSizeMeanings[sizeFor]);
+
+  return {
+    title: {
+      text: '人脉关系图',
+      top: 'bottom',
+      left: 'right'
+    },
+    legend: [{
+      // selectedMode: 'single',
+      data: categories.map(function (a) {
+        return a.name;
+      })
+    }],
+    tooltip: {},
+    series: {
+      type: 'graph',
+      layout: 'force',
+      animation: false,
+      data: nodes.map(({ sourceCount, targetCount, ...node }) => ({
+        ...node,
+        symbolSize: Math.min(10, wantFollowCount ? targetCount : sourceCount) * 10,
+      })),
+      edges: edges.filter(({ mutual }) =>  wantMutual? mutual : !mutual),
+      categories,
+      roam: true,
+      edgeSymbol: [wantMutual ? 'arrow' : 'circle', 'arrow'],
+      force: {
+        repulsion: 200
+      }
+    },
+  };
+};
+
+export default compose(
+  withStateHandlers({
+    edgeFor: 0,
+    sizeFor: 0,
+  }, {
+    setEdgeFor: () => e => ({
+      edgeFor: e.nativeEvent.selectedSegmentIndex,
+    }),
+    setSizeFor: () => e => ({
+      sizeFor: e.nativeEvent.selectedSegmentIndex,
+    }),
+  }),
+  withHandlers({ getOption }),
+)(RelationGraph);
