c40eb93fc09397fd428c42ed13a4aae4fd88052a
stupid <sunfeng32@qq.com>
2017-09-16 20:17:06 +0800

    feat(Login): fetch /login

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,8 +3,8 @@ import { compose, withHandlers, withStateHandlers } from 'recompose';
 import { Button, List, InputItem, WhiteSpace } from 'antd-mobile';
 import { createForm } from 'rc-form';
 
-const Login = ({ form: { getFieldProps }, onLogin, authenticated, error }) => (
-  <div>
+const Login = ({ form: { getFieldProps }, onLogin, authenticating, error }) => (
+  <form>
     <List renderHeader={() => '用脉脉账号登录'} style={{ width: '100%' }}>
       <InputItem
         {...getFieldProps('username')}
@@ -25,33 +25,47 @@ const Login = ({ form: { getFieldProps }, onLogin, authenticated, error }) => (
     <WhiteSpace />
     {error ? <div style={{ color: 'red' }}>{error}</div> : null}
     <WhiteSpace />
-    <Button type="primary" size="small" disabled={authenticated} onClick={onLogin}>登录</Button>
-  </div>
+    <Button type="primary" size="small" disabled={authenticating} onClick={onLogin}>登录</Button>
+  </form>
 );
 
 const onLogin = ({ form: { validateFields }, startAuth, endAuth, setError, onAuthenticated }) => () => {
-  validateFields((error, value) => {
-    console.log(error, value);
+  validateFields((error, credentials) => {
     startAuth();
-    setTimeout(() => {
-      if (Math.random() > 0.5) {
-        setError('用户名或密码错误!');
-      } else {
-        onAuthenticated();
-      }
+
+    const body = Object.keys(credentials).map((key) => {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(credentials[key]);
+    }).join('&');
+
+    fetch('/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body,
+    }).then((res) => {
       endAuth();
-    }, 1000);
+      if (res.status >= 200 && res.status < 300) {
+        onAuthenticated();
+      } else {
+        setError('用户名或密码错误!');
+      }
+    }, error => {
+      console.info(error);
+      endAuth();
+      setError('用户名或密码错误!');
+    });
   });
 };
 
 export default compose(
   createForm(),
   withStateHandlers({
-    authenticated: false,
+    authenticating: false,
     error: undefined,
   }, {
-    startAuth: () => () => ({ authenticated: true }),
-    endAuth: () => () => ({ authenticated: false }),
+    startAuth: () => () => ({ authenticating: true }),
+    endAuth: () => () => ({ authenticating: false }),
     setError: () => (error) => ({ error }),
   }),
   withHandlers({ onLogin }),
