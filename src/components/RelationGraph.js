69aa0ecb9aae8d8fa9b3e7bed75345d99a1c37ac
stupid <sunfeng32@qq.com>
2017-09-17 01:28:54 +0800

    feat(RelationGraph): with timeline`

--- src/components/RelationGraph.js
+++ src/components/RelationGraph.js
@@ -18,8 +18,14 @@ const RelationGraph = ({ getOption, setEdgeFor, setSizeFor, edgeFor, sizeFor }) 
 );
 
 const ME = 0;
-const getOption = ({ users, relationships, groups, edgeFor, sizeFor }) => () => {
-  console.log('1', relationships);
+
+const getOption = ({ getBaseOption, getChildOption }) => () => ({
+  baseOption: getBaseOption(),
+  options: [10, 35, 70, 90, 100].map(getChildOption),
+  ...console.log(getChildOption(10)),
+});
+
+const getBaseOption = ({ users, relationships, groups, edgeFor, sizeFor }) => () => {
 
   const edges = [];
   relationships.forEach((relationship) => {
@@ -63,9 +69,64 @@ const getOption = ({ users, relationships, groups, edgeFor, sizeFor }) => () => 
       layout: 'force',
       initLayout: 'circular',
       animation: true,
-      animationDuration: 1500,
+      animationDuration: 5000,
       animationEasingUpdate: 'quinticInOut',
-      data: users.map(({ size, image, isMe, ...node }) => ({
+      label: { normal: { show: true, position: 'bottom' } },
+      categories,
+      roam: true,
+      hoverAnimation: true,
+      focusNodeAdjacency: true,
+      force: {
+        repulsion: 1000,
+        gravity: 0.01,
+      },
+    },
+
+    timeline: {
+      axisType: 'category',
+      playInterval: 5000,
+      // controlStyle: {
+      //     position: 'left'
+      // },
+      data: [1,2,3,4,5],
+    },
+  };
+};
+
+const getChildOption = ({ users, relationships, groups, edgeFor, sizeFor }) => (maxId) => {
+
+  const edges = [];
+  relationships.forEach((relationship) => {
+    if (relationship.source >= maxId || relationship.target >= maxId) return;
+    if (!relationship.groups) {
+      edges.push({
+        ...relationship,
+        value: 100,
+      });
+      return;
+    }
+    const sharedGroups = relationship.groups.length ? relationship.groups : groups.slice(0, 1);
+
+    sharedGroups.forEach(sharedGroup => {
+      edges.push({
+        source: relationship.source,
+        target: sharedGroup.id - 100 + maxId,
+        value: relationship.source === ME ? 10 : 10,
+      });
+      edges.push({
+        source: sharedGroup.id - 100 + maxId,
+        target: relationship.target,
+        value: relationship.target === ME ? 10 : 10,
+      });
+    });
+  });
+
+  return {
+    title: {
+      subtext: '人脉关系图',
+    },
+    series: {
+      data: users.slice(0, maxId).map(({ size, image, isMe, ...node }) => ({
         ...node,
         symbol: `image://${image}`,
         category: isMe ? '我' : '好友',
@@ -79,17 +140,13 @@ const getOption = ({ users, relationships, groups, edgeFor, sizeFor }) => () => 
         value: 500,
       }))),
       edges,
-      label: { normal: { show: true, position: 'bottom' } },
       categories,
-      roam: true,
-      force: {
-        repulsion: 1000,
-        gravity: 0.01,
-      },
     },
   };
 };
 
+
 export default compose(
+  withHandlers({ getBaseOption, getChildOption }),
   withHandlers({ getOption }),
 )(RelationGraph);
