f86520ffa35f9d7a554429205765a4853e1448c6
stupid <sunfeng32@qq.com>
2017-09-16 20:33:12 +0800

    feat(RelationGraph): show name

--- src/components/RelationGraph.js
+++ src/components/RelationGraph.js
@@ -1,43 +1,21 @@
 import React from 'react';
-import { compose, withHandlers, withStateHandlers } from 'recompose';
+import { compose, withHandlers } from 'recompose';
 
 import ReactEcharts from 'echarts-for-react';
-import { SegmentedControl } from 'antd-mobile';
-
-const unilateralFollow = '关注';
-const mutualFollow = '互相关注';
-const followCount = '关注数量';
-const fansCount = '粉丝数量';
-const edgeMeanings = [unilateralFollow, mutualFollow];
-const nodeSizeMeanings = [followCount, fansCount];
 
 const RelationGraph = ({ getOption, setEdgeFor, setSizeFor, edgeFor, sizeFor }) => (
   <div style={{ padding: 10 }}>
     <ReactEcharts
       option={getOption()}
-      style={{ height: '700px', width: '100%' }}
+      style={{ height: '800px', width: '100%' }}
       notMerge={true}
       lazyUpdate={true}
       theme={"theme_name"}
-    />
-    <p className="sub-title">关系线表示:</p>
-    <SegmentedControl
-      values={edgeMeanings}
-      selectedIndex={edgeFor}
-      onChange={setEdgeFor}
-    />
-    <p className="sub-title">球大小表示:</p>
-    <SegmentedControl
-      values={nodeSizeMeanings}
-      selectedIndex={sizeFor}
-      onChange={setSizeFor}
     />
   </div>
 );
 
 const getOption = ({ nodes, categories, edges, edgeFor, sizeFor }) => () => {
-  const wantMutual = edgeMeanings[edgeFor] === mutualFollow;
-  const wantFollowCount = nodeSizeMeanings[sizeFor] === followCount;
 
   return {
     title: {
@@ -47,7 +25,6 @@ const getOption = ({ nodes, categories, edges, edgeFor, sizeFor }) => () => {
     legend: [{
       top: 'bottom',
       left: 'right',
-      // selectedMode: 'single',
       data: categories.map(function (a) {
         return a.name;
       })
@@ -56,34 +33,25 @@ const getOption = ({ nodes, categories, edges, edgeFor, sizeFor }) => () => {
     series: {
       type: 'graph',
       layout: 'force',
-      animation: false,
-      data: nodes.map(({ sourceCount, targetCount, ...node }) => ({
+      animation: true,
+      animationDuration: 1500,
+      animationEasingUpdate: 'quinticInOut',
+      data: nodes.map(({ count, image, ...node }) => ({
         ...node,
-        symbolSize: Math.round(Math.log(wantFollowCount ? targetCount : sourceCount)) * 10 + 10,
+        symbol: `image://${image}`,
+        symbolSize: Math.round(Math.log(count)) * 10 + 40,
       })),
-      edges: edges.filter(({ mutual }) =>  wantMutual? mutual : !mutual),
+      edges,
+      label: { normal: { show: true, position: 'bottom' } },
       categories,
       roam: true,
-      edgeSymbol: [wantMutual ? 'arrow' : 'none', 'arrow'],
-      edgeSymbolSize: 5,
       force: {
-        repulsion: 200
-      }
+        repulsion: 900,
+      },
     },
   };
 };
 
 export default compose(
-  withStateHandlers({
-    edgeFor: 0,
-    sizeFor: 0,
-  }, {
-    setEdgeFor: () => e => ({
-      edgeFor: e.nativeEvent.selectedSegmentIndex,
-    }),
-    setSizeFor: () => e => ({
-      sizeFor: e.nativeEvent.selectedSegmentIndex,
-    }),
-  }),
   withHandlers({ getOption }),
 )(RelationGraph);
--- src/utils/generateData.js
+++ src/utils/generateData.js
@@ -1,16 +1,5 @@
 import _ from 'lodash';
 
-const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-function makeName(len) {
-  let text = '';
-
-  for (let i = 0; i < len; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return text;
-}
-
 export default function() {
   const topics = _.fill(Array(50), 0).map((v, index) => {
     const positive = Math.round(Math.random() * 500);
@@ -34,46 +23,29 @@ export default function() {
   const ME = 0;
   const nodes = _.fill(Array(100), 0).map((v, index) => ({
     id: index,
-    targetCount: 0,
-    sourceCount: 0,
+    count: 0,
   }));
 
   const edges = [];
-
-  function createEdge(source, target) {
-    source.targetCount += 1;
-    target.sourceCount += 1;
-    return {
-      source: source.id,
-      target: target.id,
-    };
-  }
 
   nodes.forEach((source) => {
     nodes.forEach((target) => {
       if (target.id <= source.id) return;
 
       if (source.id === ME || Math.random() > 0.99) {
-        const mutual = Math.random() < 0.5;
-        const reverse = Math.random() < 0.5;
-        if (mutual) {
-          edges.push(createEdge(source, target));
-          edges.push(createEdge(target, source));
-          edges.push({
-            source: source.id,
-            target: target.id,
-            mutual: true,
-          });
-        } else if (reverse) {
-          edges.push(createEdge(target, source));
-        } else {
-          edges.push(createEdge(source, target));
-        }
+        edges.push({
+          source: source.id,
+          target: target.id,
+        });
+
+        source.count += 1;
+        target.count += 1;
       }
     });
   });
 
   nodes.forEach((node) => {
+    node.image = 'https://unsplash.it/100/100';
     if (node.id === ME) {
       node.name = '我';
       node.category = '我';
